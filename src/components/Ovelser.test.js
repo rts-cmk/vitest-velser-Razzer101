ed072891075beb5355d0f42f30ff868610b5a2ca
Razzer101 <Rasmus@anders-net.dk>
2025-11-10 10:33:52 +0100

    op2Done

--- src/components/Ovelser.test.js
+++ src/components/Ovelser.test.js
@@ -15,3 +15,26 @@ describe("inputType", ()=>{
         expect(inputTypes([])).toBe(false)
     })
 })
+
+function executeEverySec(func) {
+  setTimeout(func, 1000)
+}
+
+const mock = vi.fn(() => console.log('executed'))
+
+describe("grupperede tests", ()=>{
+    beforeEach(() => {
+        vi.useFakeTimers()
+    })
+    afterEach(() => {
+        vi.restoreAllMocks()
+    })
+
+    it('should execute after a second', () => {
+        executeEverySec(mock)
+        expect(mock).not.toHaveBeenCalled()
+        vi.advanceTimersToNextTimer()
+        expect(mock).toHaveBeenCalledTimes(1)
+    })
+})
+
